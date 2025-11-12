656f28077b203e7a61b6dcbe980aed59a27a9441
Pedro Augusto Menezes Rocha <pedroaugustomenezesr@gmail.com>
2025-11-12 10:21:14 -0300

    atualizando script

--- Script aula1211.R
+++ Script aula1211.R
@@ -1,6 +1,17 @@
 #importação da base
 library(readxl)
+library(ggplot2)
 Base_Casos <- read_excel("Base Casos.xlsx")
 View(Base_Casos)
 
-boxplot(Base_Casos$Estatura)
+#boxplot
+Base_Casos |> 
+  ggplot(mapping = aes(y = Estatura)) +
+  geom_boxplot(fill = "red")
+
+#histograma
+
+Base_Casos |> 
+  ggplot(mapping = aes(x = Idade)) +
+  geom_histogram(fill = "black")
+
